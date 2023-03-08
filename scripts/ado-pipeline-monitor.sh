4611e12b08d706de9026440f89a7bfb2bf1932e1
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-03-08 12:38:22 +0000

    typo

--- scripts/ado-pipeline-monitor.sh
+++ scripts/ado-pipeline-monitor.sh
@@ -5,25 +5,30 @@ ADO_PROJECT=$2
 ADO_DEFINITION_ID=$3
 HOURS_FOR_AMBER=$4
 HOURS_FOR_RED=$5
+PIPELINE_NAME=$6
 
 
-MIN_TIME_RED=$(date -d "-${HOURS_FOR_RED} Hours" +"%Y-%m-%dT%H:%M:%SZ" )
+PIPELINE_MESSAGE="<https://dev.azure.com/hmcts/$ADO_PROJECT/_build?definitionId=$ADO_DEFINITION_ID|$PIPELINE_NAME pipeline>"
+
+#MIN_TIME_RED=$(date -v "-${HOURS_FOR_RED}H" +"%Y-%m-%dT%H:%M:%SZ" )
+MIN_TIME_RED=$(date -v "-${HOURS_FOR_RED}H" +"%Y-%m-%dT%H:%M:%SZ" )
 RESULT=$(curl -u :$ADO_TOKEN "https://dev.azure.com/hmcts/$ADO_PROJECT/_apis/build/builds?api-version=7.0&definitions=$ADO_DEFINITION_ID&resultFilter=succeeded&\$top=1&minTime=$MIN_TIME_RED")
 COUNT=$(jq -r .count <<< "${RESULT}")
 
 if [ "$COUNT" != 1 ]; then
-  echo ":red: Pipeline didn't have a successful run in last $HOURS_FOR_RED hours." >> slack-message.txt
+  echo ":red: $PIPELINE_MESSAGE didn't have a successful run in last $HOURS_FOR_RED hours." >> slack-message.txt
   exit 0
 fi
 
+#MIN_TIME_AMBER=$(date -v "-${HOURS_FOR_AMBER}H" +"%Y-%m-%dT%H:%M:%SZ" )
 MIN_TIME_AMBER=$(date -d "-${HOURS_FOR_AMBER} Hours" +"%Y-%m-%dT%H:%M:%SZ" )
 RESULT=$(curl -u :$ADO_TOKEN "https://dev.azure.com/hmcts/$ADO_PROJECT/_apis/build/builds?api-version=7.0&definitions=$ADO_DEFINITION_ID&resultFilter=succeeded&\$top=1&minTime=$MIN_TIME_AMBER")
 COUNT=$(jq -r .count <<< "${RESULT}")
 
 if [ "$COUNT" != 1 ]; then
-  echo ":amber: Pipeline didn't have a successful run in last $HOURS_FOR_AMBER hours." >> slack-message.txt
+  echo ":amber: $PIPELINE_MESSAGE didn't have a successful run in last $HOURS_FOR_AMBER hours." >> slack-message.txt
   exit 0
 fi
 
-echo ":green: Pipeline had a successful run in last $HOURS_FOR_AMBER hours." >> slack-message.txt
+echo ":green: $PIPELINE_MESSAGE had a successful run in last $HOURS_FOR_AMBER hours." >> slack-message.txt
 
