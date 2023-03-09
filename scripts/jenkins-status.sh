68207e77f1883c05ebca13418c565e431f3dcfbe
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-03-09 17:36:43 +0000

    Add build queue

--- scripts/jenkins-status.sh
+++ scripts/jenkins-status.sh
@@ -4,6 +4,20 @@ JENKINS_URL=$3
 
 printf "\n:jenkins: <https://build.platform.hmcts.net/|_*Jenkins Status*_> \n\n" >> slack-message.txt
 
+BUILD_QUEUE_RESULT=$( curl -u "$JENKINS_USERNAME":"$JENKINS_API_TOKEN" "$JENKINS_URL/queue/api/json")
+BUILD_QUEUE_COUNT=$(jq -r '.items | length' <<< "$BUILD_QUEUE_RESULT")
+
+BUILD_QUEUE_STATUS=":red_circle:"
+if (( "$BUILD_QUEUE_COUNT" <= 75 )); then
+  BUILD_QUEUE_STATUS=":green_circle:"
+elif (( "$BUILD_QUEUE_COUNT" <= 125 )); then
+  BUILD_QUEUE_STATUS=":yellow_circle:"
+fi
+
+printf ">\n> _%s Build Queue :_ *%s* :sign-queue: \n" "$BUILD_QUEUE_STATUS" "$BUILD_QUEUE_COUNT" >> slack-message.txt
+
+printf ">\n> Dashboard Status:_  \n" >> slack-message.txt
+
 DASHBOARD_RESULT=$( curl -u $JENKINS_USERNAME:$JENKINS_API_TOKEN "$JENKINS_URL/view/Platform/api/json?depth=1")
 
 count=$(jq -r '.jobs | length' <<< $DASHBOARD_RESULT)
