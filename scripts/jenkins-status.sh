f006fedb8a18878180d921c909468d2b17144af3
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-03-09 17:41:27 +0000

    fix

--- scripts/jenkins-status.sh
+++ scripts/jenkins-status.sh
@@ -14,9 +14,9 @@ elif (( "$BUILD_QUEUE_COUNT" <= 125 )); then
   BUILD_QUEUE_STATUS=":yellow_circle:"
 fi
 
-printf ">\n> _%s Build Queue :_ *%s* :sign-queue: \n" "$BUILD_QUEUE_STATUS" "$BUILD_QUEUE_COUNT" >> slack-message.txt
+printf ">_%s Build Queue :_ *%s* :sign-queue: \n" "$BUILD_QUEUE_STATUS" "$BUILD_QUEUE_COUNT" >> slack-message.txt
 
-printf ">\n> Dashboard Status:_  \n" >> slack-message.txt
+printf ">\n> _Dashboard Status:_  \n" >> slack-message.txt
 
 DASHBOARD_RESULT=$( curl -u $JENKINS_USERNAME:$JENKINS_API_TOKEN "$JENKINS_URL/view/Platform/api/json?depth=1")
 
