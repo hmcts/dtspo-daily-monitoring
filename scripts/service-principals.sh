237a96477c5bab833b23fe0109920e4ea2ea5ef0
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-04-03 14:18:26 +0100

    pushing delete secret file to keep copy on commit

--- scripts/service-principals.sh
+++ scripts/service-principals.sh
@@ -7,9 +7,8 @@ TODAY_DATE=$(date +%Y-%m-%d)
 CHECK_DATE=$(date -d "+${CHECK_DAYS} days" +%Y-%m-%d)
 
 
-AZ_APP_RESULT=$( az ad app list --all --query "[?passwordCredentials[?endDateTime > '${TODAY_DATE}' && endDateTime < '${CHECK_DATE}']].{displayName:displayName, appId:appId, createdDateTime:createdDateTime, passwordCredentials:passwordCredentials[?endDateTime > '${TODAY_DATE}' && endDateTime < '${CHECK_DATE}'].{displayName:displayName,endDateTime:endDateTime}}" --output json )
+AZ_APP_RESULT=$( az ad app list --all --query "[?passwordCredentials[?endDateTime < '${CHECK_DATE}']].{displayName:displayName, appId:appId, createdDateTime:createdDateTime, passwordCredentials:passwordCredentials[?endDateTime < '${CHECK_DATE}'].{displayName:displayName,endDateTime:endDateTime}}" --output json )
 
-#AZ_APP_RESULT=$( az ad app list --all --query "[?passwordCredentials[?endDateTime > '2023-03-28' && endDateTime < '2023-04-29']].{displayName:displayName, appId:appId, createdDateTime:createdDateTime, passwordCredentials:passwordCredentials[?endDateTime > '2023-03-28' && endDateTime < '2023-04-29'].{displayName:displayName,endDateTime:endDateTime}}" --output json )
 
 AZ_APP_COUNT=$(jq -r '. | length' <<< "${AZ_APP_RESULT}")
 
@@ -25,7 +24,7 @@ fi
 echo "$AZ_APP_RESULT" | jq -c -r '.[]'  | while read i; do
     displayName=$(jq -r '.displayName' <<< "$i")
     appId=$(jq -r '.appId' <<< "$i")
-    endDateTime=$(jq -r '.passwordCredentials[].endDateTime' <<< "$i")
+    endDateTime=$(jq -r '.passwordCredentials[0].endDateTime' <<< "$i")
     
     convert_date=$(date -d "$endDateTime" +%Y-%m-%d)
     date_diff=$(( ($(date -d "$convert_date UTC" +%s) - $(date -d "UTC" +%s) )/(60*60*24) ))
@@ -33,9 +32,9 @@ echo "$AZ_APP_RESULT" | jq -c -r '.[]'  | while read i; do
 
     APP_URL="https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Credentials/appId/$appId"
     if [[ $((date_diff)) -gt 7 ]]; then
-        printf "\n:yellow_circle: <$APP_URL|_* $displayName *_> Secret Expiring in next $date_diff days \n\n" >> slack-message.txt
+        printf "\n:yellow_circle: <$APP_URL|_* $displayName*_> expires in $date_diff days \n\n" >> slack-message.txt
     else
-        printf "\n:red_circle: <$APP_URL|_* $displayName *_> Secret Expiring in next $date_diff days \n\n" >> slack-message.txt
+        printf "\n:red_circle: <$APP_URL|_* $displayName*_> expires in $date_diff days \n\n" >> slack-message.txt
     fi
 
 done
