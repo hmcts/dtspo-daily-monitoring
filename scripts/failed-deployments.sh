d0bb006abb08147e9c2078029123e3726d4a254d
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2023-06-13 09:20:48 +0100

    Update output text

--- scripts/failed-deployments.sh
+++ scripts/failed-deployments.sh
@@ -35,7 +35,8 @@ do
 
     # Check if the deployment has failed (ready replicas are less than desired replicas)
     if [[ "$ready_replicas" != "$desired_replicas" ]]; then
-      printf "Deployment \`%s\` in namespace \`%s\` on *%s* has failed\n" "$deployment_name" "$namespace" "$CLUSTER_NAME" >> failed-deployments.txt
+      printf "\n:flux: *Failed Delpoyments on ^^$CLUSTER_NAME* \n\n" >> failed-deployments.txt
+      echo ":x: Deployment \`%s\` in namespace \`%s\` has failed\n" "$deployment_name" "$namespace" >> failed-deployments.txt
 
       bash scripts/failed-deployments-slack.sh "$WEBHOOK_URL" "$SLACKCHANNEL"
     fi
