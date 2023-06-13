9ee4e3c005fbc9bfadcc7bed0a4c80b9c8c8575a
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2023-06-13 14:53:00 +0100

    Only fire the slack message once

--- scripts/failed-deployments.sh
+++ scripts/failed-deployments.sh
@@ -7,7 +7,7 @@ CLUSTER_NAME="${3}"
 SLACKCHANNEL="${4}"
 WEBHOOK_URL="${5}"
 
-echo  "Get aks credentials "
+echo "Get AKS credentials"
 
 az account set --subscription ${SUBSCRIPTION}
 az aks get-credentials \
@@ -30,29 +30,28 @@ touch "$output_file"
 namespaces=$(kubectl get namespaces --no-headers=true | awk '{print $1}')
 
 # Loop through each namespace
-for namespace in $namespaces
-do
-  echo "Checking deployments in namespace: $namespace"
+for namespace in $namespaces; do
+    echo "Checking deployments in namespace: $namespace"
 
-  # Get the list of deployments in the namespace
-  deployments=$(kubectl get deployments -n "$namespace" --no-headers=true)
+    # Get the list of deployments in the namespace
+    deployments=$(kubectl get deployments -n "$namespace" --no-headers=true)
 
-  # Loop through each deployment
-  while read -r line; do
-    deployment_name=$(echo "$line" | awk '{print $1}')
-    ready_replicas=$(echo "$line" | awk '{print $2}' | awk -F'/' '{print $1}')
-    desired_replicas=$(echo "$line" | awk '{print $2}' | awk -F'/' '{print $2}')
+    # Loop through each deployment
+    while read -r line; do
+        deployment_name=$(echo "$line" | awk '{print $1}')
+        ready_replicas=$(echo "$line" | awk '{print $2}' | awk -F'/' '{print $1}')
+        desired_replicas=$(echo "$line" | awk '{print $2}' | awk -F'/' '{print $2}')
 
-    # Check if the deployment has failed (ready replicas are less than desired replicas)
-    if [[ "$ready_replicas" != "$desired_replicas" ]]; then
-    # Check if the deployment has already been processed
-      if ! grep -Fxq "$deployment_name" "${output_file}"; then
-      printf "\n:flux: Deployment \`%s\` in namespace \`%s\` on *%s* has failed\n" "$deployment_name" "$namespace" "${CLUSTER_NAME^^}" >> "$output_file"
-    # Execute slack message script
-      bash scripts/failed-deployments-slack.sh "$WEBHOOK_URL" "$SLACKCHANNEL"
-      fi
-    fi
-  done <<< "$deployments"
+        # Check if the deployment has failed (ready replicas are less than desired replicas)
+        if [[ "$ready_replicas" != "$desired_replicas" ]]; then
+            printf "\n:flux: Deployment \`%s\` in namespace \`%s\` on *%s* has failed\n" "$deployment_name" "$namespace" "${CLUSTER_NAME^^}" >> "$output_file"
+        fi
+    done <<<"$deployments"
 
-  echo
+    echo
 done
+
+# Send the failed deployment message to Slack if there are any failures
+if [[ -s "$output_file" ]]; then
+    bash scripts/failed-deployments-slack.sh "$WEBHOOK_URL" "$SLACKCHANNEL" < "$output_file"
+fi
