a18195bff37607ad2be0375d37f769be9ea279f4
Khaled Siddat <khaled.siddat@justice.gov.uk>
2023-06-19 12:06:06 +0100

    adding cert file

---
+++ scripts/certcheck.sh
@@ -0,0 +1,30 @@
+# Azure CLI command to populate URL list
+subscription_id=$1
+resource_group=$2
+front_door_name=$3
+
+# Function to check certificate expiration
+check_certificate_expiration() {
+    url=$1
+    expiration_date=$(echo | openssl s_client -servername "${url}" -connect "${url}:443" 2>/dev/null | openssl x509 -noout -dates 2>/dev/null | grep "notAfter" | cut -d "=" -f 2)
+
+    if [[ -n $expiration_date ]]; then
+        expiration_timestamp=$(gdate -d "${expiration_date}" +%s)
+        current_timestamp=$(gdate +%s)
+        seconds_left=$((expiration_timestamp - current_timestamp))
+        days_left=$((seconds_left / 86400))
+
+        if [[ $days_left -le 100 ]]; then
+            echo "Certificate for ${url} expires in ${days_left} days."
+        fi
+    else
+        echo "Unable to check the certificate for ${url}."
+    fi
+}
+
+urls=$(az network front-door frontend-endpoint list --subscription "$subscription_id" --resource-group "$resource_group" --front-door-name "$front_door_name" --query "[].hostName" -o tsv)
+
+# Check certificate expiration for each URL
+for url in $urls; do
+    check_certificate_expiration "${url}"
+done
