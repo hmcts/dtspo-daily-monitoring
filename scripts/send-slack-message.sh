b43418893d416d0ced4ba03d45a9312c3b9748e7
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-07-13 16:24:41 +0100

    try escaping

--- scripts/send-slack-message.sh
+++ scripts/send-slack-message.sh
@@ -3,7 +3,5 @@
 WEBHOOK_URL=$1
 CHANNEL_NAME=$2
 
-cat slack-message.txt
-
 MESSAGE=$(cat slack-message.txt)
 curl -s -X POST --data-urlencode "payload={\"channel\": \"${CHANNEL_NAME}\", \"username\": \"Plato\", \"text\": \"${MESSAGE}\", \"icon_emoji\": \":plato:\"}" ${WEBHOOK_URL}
