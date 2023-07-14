118e44983e17f7828955d7b20a909e42c93fdcf0
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-07-14 10:14:15 +0100

    test pulling user

--- scripts/send-slack-message.sh
+++ scripts/send-slack-message.sh
@@ -2,6 +2,9 @@
 
 WEBHOOK_URL=$1
 CHANNEL_NAME=$2
+BUILD_QUEUED_BY=$3
+
+echo $BUILD_QUEUED_BY
 
 MESSAGE=$(cat slack-message.txt)
 curl -s -X POST --data-urlencode "payload={\"channel\": \"${CHANNEL_NAME}\", \"username\": \"Plato\", \"text\": \"${MESSAGE}\", \"icon_emoji\": \":plato:\"}" ${WEBHOOK_URL}
