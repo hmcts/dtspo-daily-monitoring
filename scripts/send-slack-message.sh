31813c6f93677834ef567feeaa0ab3b87b3095ee
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-07-14 13:52:13 +0100

    add slack script for pr

--- scripts/send-slack-message.sh
+++ scripts/send-slack-message.sh
@@ -2,9 +2,6 @@
 
 WEBHOOK_URL=$1
 CHANNEL_NAME=$2
-BUILD_REQUESTED_FOR=$3
-
-echo $BUILD_REQUESTED_FOR
 
 MESSAGE=$(cat slack-message.txt)
 curl -s -X POST --data-urlencode "payload={\"channel\": \"${CHANNEL_NAME}\", \"username\": \"Plato\", \"text\": \"${MESSAGE}\", \"icon_emoji\": \":plato:\"}" ${WEBHOOK_URL}
