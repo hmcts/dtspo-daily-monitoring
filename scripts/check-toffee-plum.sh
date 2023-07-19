2814b5e839d147989a168dab44b0f27aadf3861c
Louise Huyton <louise.huyton@justice.gov.uk>
2023-07-19 17:26:38 +0100

    change task to bash and redo script

--- scripts/check-toffee-plum.sh
+++ scripts/check-toffee-plum.sh
@@ -1,70 +1,40 @@
 #!/usr/bin/env bash
-function subscription () {
-   
-        SUBSCRIPTION_ID=$(jq -r '.id' <<< $subscription)
-        az account set -s $SUBSCRIPTION_ID
-        CLUSTERS=$(az resource list \
-        --resource-type Microsoft.ContainerService/managedClusters \
-        --query "[?tags.application == 'true']" -o json)
-}
 
-function cluster () {
-        RESOURCE_GROUP=$(jq -r '.resourceGroup' <<< $cluster)
-        NAME=$(jq -r '.name' <<< $cluster)
+ENVIRONMENTS=('sbox', 'ithc', 'demo', 'prod')
+
+function environments() {
+        if [[ "$APP" == "toffee" ]]; then
+        $ENVIRONMENTS+=('dev', 'test', 'staging')
+        elif [[ "$APP" == "plum" ]]; then
+        $ENVIRONMENTS+=('preview', 'perftest', 'aat')
+        fi
 }
 
 function logic () {
+    statuscode=$(curl --max-time 30 --retry 20 --retry-delay 15 -s -o /dev/null -w "%{http_code}"  https://$APP.$ENV.platform.hmcts.net)
+
     if [[ "$ENVIRONMENT" == "demo" && $statuscode -eq 302 ]]; then
-        printf "\n>:green_circle: ttps://$APPLICATION.platform.hmcts.net" >> slack-message.txt
+        printf "\n>:green_circle: https://$APP.$ENV.platform.hmcts.net" >> slack-message.txt
     elif [[ $statuscode -eq 200 ]]; then
-        printf "\n>:green_circle: ttps://$APPLICATION.platform.hmcts.net" >> slack-message.txt
+        printf "\n>:green_circle: https://$APP.$ENV.platform.hmcts.net" >> slack-message.txt
     else
-        printf "\n>:red_circle: ttps://$APPLICATION.platform.hmcts.net" >> slack-message.txt
+        printf "\n>:red_circle: https://$APP.$ENV.platform.hmcts.net" >> slack-message.txt
     fi
 }
 
+function uptime () {
+    for $ENV in "${ENVIRONMENTS[@]}"
+        do
+        logic()
+        done
+}
 
-SUBSCRIPTIONS=$(az account list -o json)
-jq -c '.[]' <<< $SUBSCRIPTIONS | while read subscription; do
-subscription
-    jq -c '.[]' <<< $CLUSTERS | while read cluster; do
-        cluster
+### test toffee
+APP="toffee"
+environments()
+uptime()
 
-        BUSINESS_AREA=$(jq -r '.tags.businessArea' <<< $cluster)
-        if [[ "$BUSINESS_AREA" == "Cross-Cutting" ]]; then
-            APP="toffee"
-        elif [[ "$BUSINESS_AREA" == "CFT" ]]; then
-            APP="plum"
-        fi
-
-        ENVIRONMENT=$(jq -r '.tags.environment' <<< $cluster)
-
-        if [[ "$ENVIRONMENT" == "sandbox" || "$ENVIRONMENT" == "Sandbox" ]]; then
-            ENV="sbox"
-        elif [[ "$ENVIRONMENT" == "testing" ]]; then
-            ENV="perftest"
-        else
-            ENV="$ENVIRONMENT"
-        fi
-
-        ts_echo "Test that $APP works in $ENVIRONMENT after $NAME start-up"
-        if [[ "$ENVIRONMENT" == "testing" && "$APP" == "toffee" ]]; then
-            APPLICATION="$APP.test"
-        elif [[ "$ENVIRONMENT" == "testing" && "$APP" == "plum" ]]; then
-            APPLICATION="$APP.perftest"
-        else 
-            APPLICATION="$APP.$ENVIRONMENT"
-        fi
-
-        statuscode=$(curl --max-time 30 --retry 20 --retry-delay 15 -s -o /dev/null -w "%{http_code}"  https://$APPLICATION.platform.hmcts.net)
-
-        if [[ "$APP" == "toffee" ]]; then
-            logic
-        elif [[ "$APP" == "plum" ]]; then
-            logic
-        fi
-
-
-        
-    done
-done
+### test plum
+APP="plum"
+environments()
+uptime()
