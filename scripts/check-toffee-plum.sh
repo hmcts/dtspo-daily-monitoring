6a68ee8a0f6fc7727dce279f60fd7cd451350838
Louise Huyton <louise.huyton@justice.gov.uk>
2023-07-24 11:45:47 +0100

    basic logic for loop

--- scripts/check-toffee-plum.sh
+++ scripts/check-toffee-plum.sh
@@ -10,19 +10,19 @@ function add_environments() {
 
 function logic() {
     statuscode=$(curl --max-time 30 --retry 20 --retry-delay 15 -s -o /dev/null -w "%{http_code}"  https://$APP.$ENV.platform.hmcts.net)
-
-    if [[ "$ENVIRONMENT" == "demo" && $statuscode -eq 302 ]]; then
-        printf "\n>:green_circle: https://$APP.$ENV.platform.hmcts.net" >> slack-message.txt
-    elif [[ $statuscode -eq 200 ]]; then
-        printf "\n>:green_circle: https://$APP.$ENV.platform.hmcts.net" >> slack-message.txt
-    else
-        printf "\n>:red_circle: https://$APP.$ENV.platform.hmcts.net" >> slack-message.txt
-    fi
+    echo $statuscode
+    # if [[ "$ENVIRONMENT" == "demo" && $statuscode -eq 302 ]]; then
+    #     printf "\n>:green_circle: https://$APP.$ENV.platform.hmcts.net" >> slack-message.txt
+    # elif [[ $statuscode -eq 200 ]]; then
+    #     printf "\n>:green_circle: https://$APP.$ENV.platform.hmcts.net" >> slack-message.txt
+    # else
+    #     printf "\n>:red_circle: https://$APP.$ENV.platform.hmcts.net" >> slack-message.txt
+    # fi
 }
 
 function uptime() {
 for env in ${ENVIRONMENTS[@]}; do
-  echo "hi there"
+  logic
 done
 }
 
