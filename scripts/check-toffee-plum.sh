1b24e2a5c79e9582666fce36647dedd33bf65627
Louise Huyton <louise.huyton@justice.gov.uk>
2023-08-02 16:12:45 +0100

    change slack_message() function

--- scripts/check-toffee-plum.sh
+++ scripts/check-toffee-plum.sh
@@ -32,8 +32,8 @@ function slack_message() {
     if [[ $failed -eq false ]]; then
         printf "\n>:green_circle:  All $APP environments are accessible" >> slack-message.txt  
         # printf "\n>:red_circle:  <$url| $ENV>" >> slack-message.txt  
-    # else
-    #     printf "\n>:green_circle:  <$url| $ENV>" >> slack-message.txt
+    else
+        printf "\n>:green_circle:  <$url| $ENV>" >> slack-message.txt
     fi
 }
 
