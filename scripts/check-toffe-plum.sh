5f960eb893c63aed25809308b7652a1d635b50c0
Jordan Hoey <jordan.hoey@justice.gov.uk>
2023-08-03 14:23:49 +0100

    testing with Louise

---
+++ scripts/check-toffe-plum.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+function add_environments() {
+        if [[ "$1" == "toffee" ]]; then
+        ENVIRONMENTS=("sandbox" "test" "ithc" "demo" "staging" "prod" "yay")
+        fi
+        if [[ "$1" == "plum" ]]; then
+        ENVIRONMENTS=("sandbox" "perftest" "ithc" "demo" "aat" "prod")
+        fi
+}
+
+function status_code() {
+    if [ $ENV == "prod" ]; then
+        url="https://$1.platform.hmcts.net"
+        statuscode=$(curl -s -o /dev/null -w "%{http_code}" $url)
+    elif [ $ENV != "prod" ]; then
+        url="https://$1.$ENV.platform.hmcts.net"
+        statuscode=$(curl -s -o /dev/null -w "%{http_code}" $url)
+    fi
+}
+
+function failure_check() {
+    if [[ $statuscode != 200 ]] && [[ $1 == "toffee" ]]; then
+        failures_exist_toffee="true"
+        printf "\n>:red_circle:  <$url| $ENV>" #>> slack-message.txt
+    elif [[ $statuscode != 200 ]] && [[ $1 == "plum" ]]; then
+        failures_exist_plum="true"
+        printf "\n>:red_circle:  <$url| $ENV>" #>> slack-message.txt
+    fi
+}
+
+function uptime() {
+for ENV in ${ENVIRONMENTS[@]}; do
+    status_code $1
+    failure_check $1
+done
+}
+
+function do_failures_exist() {
+    if [[ $1 = "toffee" ]]; then
+        echo "toffee failures var value is $failures_exist_toffee"
+        if [[ failures_exist_toffee != "true" ]]; then
+            printf "\n>:green_circle: EVERYTHING WORKED in toffee - THIS IS A TEST"
+        fi
+    elif [[ $1 = "plum" ]]; then
+        if [[ failures_exist_plum != "true" ]]; then
+            printf "\n>:green_circle: EVERYTHING WORKED in plum"
+        fi
+    fi
+}
+
+printf "\n:detective-pikachu: _*Check Toffee/Plum Status*_ \n" #>> slack-message.txt
+
+APPS=("toffee" "plum")
+    for APP in ${APPS[@]}; do
+    printf "\n*$APP Status:*" #>> slack-message.txt
+
+    add_environments $APP
+    uptime $APP
+    do_failures_exist $APP
+done
+
