4464464b1cf72e26b04e6af2a5716fe479305b9a
JusticeCarl <133108937+JusticeCarl@users.noreply.github.com>
2023-08-07 17:37:26 +0100

    changed wile loops to iterate over tabs rather than new lines as jq in bash behaves differently to zsh

--- scripts/github-scheduled-workflow-monitor.sh
+++ scripts/github-scheduled-workflow-monitor.sh
@@ -1,4 +1,4 @@
-#JH TESTING
+#!/bin/bash
 # This script expects the following options:
 # Github Api Token
 token=$1
@@ -11,75 +11,60 @@ run=$4
 # Github owner 
 owner=hmcts
 
-
-
-
-
 # a function to make it easier to print the message usage: message [red | yellow | green]
 print_message() {
     printf "> :${1}_circle: Workflow name: <${html_url}|_*${name}*_> Workflow status: ${workflow_status} Workflow conclusion: ${conclusion} Started at: ${run_started_at} \n" >> slack-message.txt
 }
-
-# echo "TESTING"
-# html_url=https://abc
-# name=test
-# workflow_status=test
-# conclusion=test
-# run_started_at=test
-# run=test
-# print_message red  >> slack-message.txt 
-
-
-# printf "\n:GitHub sheduled Workflow status: <https://https://github.com/${owner}/> \n\n" >> slack-message.txt
-
 printf "\n:Github Sheduled Workflow Status: <https://github.com/${owner}|_*GitHub Workflow Status*_> \n\n" >> slack-message.txt
 
-# Check if we need to intergoate a specific run or all of the runs for that workflow
-
+# Check if we need to intergoate a specific run or all of the runs for that workflow 
 if [[ -z "${run}" ]];
 then
-    echo "found run working on ${run}"
+    echo "----"
+    echo "Run not defined loop through all runs in workflows"
     # no specific run specified therefore run for each workflow in the repo
     # loop through list of workflows in repo to monitor
     workflows_response=$(curl -s -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${token}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/${owner}/${repo}/actions/workflows | jq -r '.workflows[] | [.id , .name] |@csv')
-    echo "WORKFLOW RESPONSE"
+    echo "WORKFLOW RESPONSE:"
     echo ${workflows_response}
     # While loop reads in data piped to it.
+    IFS=$'\t'
     while read -r w;
     do    
+        echo "Working on: "$w
         # loop through the $workflows_respose and for each of the workflows find their name and id   
         while IFS=, read -r id name;
         do
-            echo "ID is ${id}"
-            echo "Name is ${name}"
-            # Get the last response for that workflow id and specified branch 
-            workflow_status=$(curl -s -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${token}" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/${owner}/${repo}/actions/workflows/${id}/runs?per_page=1&branch=$branch" | jq -r '.workflow_runs[] | [.status , .conclusion , .html_url , .run_started_at] |@csv')
+            workflow_status=$(curl -s -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${token}" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/${owner}/${repo}/actions/workflows/${id}/runs?per_page=1&branch=${branch}" | jq -r '.workflow_runs[] | [.status , .conclusion , .html_url , .run_started_at] |@csv')
             # While loop reads in data piped to it.
             echo "Workflow status is: "
             echo ${workflow_status}
-
             while IFS=, read -r workflow_status conclusion html_url run_started_at;
             do
                 # clean up responses
                 workflow_status=$(echo ${workflow_status} | tr -d '"')
-                conclusion=$(echo ${conclusion} |tr -d '"')
-                run_started_at=$(echo ${run_started_at} | sed -e 's/T/ /; s/Z//')
-                echo wf_status:${workflow_status}
-                echo conclusion:${conclusion}
-                echo runstarted:${run_started_at}
-
-                # Write slack message dependant on status and conclusion
-                if [ "${conclusion}" = "success" ];
+                if [ -z ${workflow_status} ];
                 then
-                    print_message green
-                elif [[ "${workflow_status}" == "waiting" ]] | [[ "${workflow_status}" == "pending" ]] | [[ "${workflow_status}" == "in_progress" ]] | [[ "${workflow_status}" == "queued" ]] | [[ "${workflow_status}" == "waiting" ]]
-                then
-                    print_message yellow
-                else 
-                    print_message red
-                fi    
+                    printf "> :red_circle: Workflow name: <"https://github.com/${owner}/${repo}/actions/runs/"|_*${name}*_> did not return a workflow status" >> slack-message.txt
+                else
+                    conclusion=$(echo ${conclusion} |tr -d '"')
+                    run_started_at=$(echo ${run_started_at} | sed -e 's/T/ /; s/Z//')
+                    echo wf_status:${workflow_status}
+                    echo conclusion:${conclusion}
+                    echo runstarted:${run_started_at}
+                    # Write slack message dependant on status and conclusion
+                    if [ "${conclusion}" = "success" ];
+                    then
+                        print_message green
+                    elif [[ "${workflow_status}" == "waiting" ]] | [[ "${workflow_status}" == "pending" ]] | [[ "${workflow_status}" == "in_progress" ]] | [[ "${workflow_status}" == "queued" ]] | [[ "${workflow_status}" == "waiting" ]]
+                    then
+                        print_message yellow
+                    else 
+                        print_message red
+                    fi    
+                 fi    
             done  <<< ${workflow_status}
-        done <<< $w 
+        done <<< $w
     done <<< ${workflows_response}
 else
     # A specific workflow run was specified interogate only that one and get its id
@@ -98,19 +83,27 @@ else
             do
                 # clean up responses
                 workflow_status=$(echo ${workflow_status} | tr -d '"')
-                conclusion=$(echo ${conclusion} |tr -d '"')
-                run_started_at=$(echo ${run_started_at} | sed -e 's/T/ /; s/Z//')
-
-                # Write slack message dependant on status and conclusion
-                if [ "${conclusion}" = "success" ];
+                echo wf_status:${workflow_status}
+                if [ -z ${workflow_status} ];
                 then
-                    print_message green
-                elif [[ "${workflow_status}" == "waiting" ]] | [[ "${workflow_status}" == "pending" ]] | [[ "${workflow_status}" == "in_progress" ]] | [[ "${workflow_status}" == "queued" ]] | [[ "${workflow_status}" == "waiting" ]]
-                then
-                    print_message yellow
-                else 
-                    print_message red
-                fi    
+                    printf "> :${1}_circle: Workflow name: <${html_url}|_*${name}*_> did not return a workflow status" >> slack-message.txt
+                else     
+                    conclusion=$(echo ${conclusion} |tr -d '"')
+                    run_started_at=$(echo ${run_started_at} | sed -e 's/T/ /; s/Z//')
+                    
+                    echo conclusion:${conclusion}
+                    echo runstarted:${run_started_at}
+                    # Write slack message dependant on status and conclusion
+                    if [ "${conclusion}" = "success" ];
+                    then
+                        print_message green
+                    elif [[ "${workflow_status}" == "waiting" ]] | [[ "${workflow_status}" == "pending" ]] | [[ "${workflow_status}" == "in_progress" ]] | [[ "${workflow_status}" == "queued" ]] | [[ "${workflow_status}" == "waiting" ]]
+                    then
+                        print_message yellow
+                    else 
+                        print_message red
+                    fi    
+                fi
             done  <<< ${workflow_status}
         done <<< ${workflows_response_id}
 fi
