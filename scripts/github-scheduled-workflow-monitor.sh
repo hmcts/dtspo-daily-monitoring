f3dc9bea07d81dc1394c3887db6c1a976038c7ce
JusticeCarl <133108937+JusticeCarl@users.noreply.github.com>
2023-08-07 19:37:57 +0100

    removed write to slack message to testing

--- scripts/github-scheduled-workflow-monitor.sh
+++ scripts/github-scheduled-workflow-monitor.sh
@@ -13,11 +13,12 @@ owner=hmcts
 
 # a function to make it easier to print the message usage: message [red | yellow | green]
 print_message() {
-    printf "> :${1}_circle: Workflow name: <${html_url}|${name}> Workflow status: ${workflow_status} Workflow conclusion: ${conclusion} Started at: ${run_started_at} \n" >> slack-message.txt
+    # printf "\n>:red_circle:  *$vm_name* backup in <https://portal.azure.com/#@HMCTS.NET/resource$parsed_vault_url|_*$VAULT_NAME*_> has $job_status" >> slack-message.txt
+    printf "\n>:${1}_circle: Workflow name: <${html_url}|${name}> Workflow status: ${workflow_status} Workflow conclusion: ${conclusion} Started at: ${run_started_at} \n" #>> slack-message.txt
 }
 
 # printf "\n:github: <https://github.com/orgs/hmcts/people|_*GitHub License Status*_> \n\n" >> slack-message.txt
-printf "\n :github: Sheduled Workflow Status: \n" >> slack-message.txt
+# printf "\n :github: Sheduled Workflow Status: \n" >> slack-message.txt
 
 # Check if we need to intergoate a specific run or all of the runs for that workflow 
 if [[ -z "${run}" ]];
@@ -47,7 +48,7 @@ then
                 workflow_status=$(echo ${workflow_status} | tr -d '"')
                 if [ -z ${workflow_status} ];
                 then
-                    printf "> :red_circle: Workflow name: <"https://github.com/${owner}/${repo}/actions/runs/"|_*${name}*_> did not return a workflow status" >> slack-message.txt
+                    printf "> :red_circle: Workflow name: <"https://github.com/${owner}/${repo}/actions/runs/"|_*${name}*_> did not return a workflow status" #>> slack-message.txt
                 else
                     conclusion=$(echo ${conclusion} |tr -d '"')
                     run_started_at=$(echo ${run_started_at} | sed -e 's/T/ /; s/Z//')
