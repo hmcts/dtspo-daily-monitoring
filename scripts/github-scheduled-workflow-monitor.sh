64aef00e3fb57ffeceb3838e3e2d36d7562a29ee
JusticeCarl <133108937+JusticeCarl@users.noreply.github.com>
2023-08-07 18:52:26 +0100

    changes primary message

--- scripts/github-scheduled-workflow-monitor.sh
+++ scripts/github-scheduled-workflow-monitor.sh
@@ -15,7 +15,8 @@ owner=hmcts
 print_message() {
     printf "> :${1}_circle: Workflow name: <${html_url}|${name}> Workflow status: ${workflow_status} Workflow conclusion: ${conclusion} Started at: ${run_started_at} \n" >> slack-message.txt
 }
-printf "\n:Github Sheduled Workflow Status: \n\n" >> slack-message.txt
+
+printf "\n _:Github: *Sheduled Workflow Status:*_ \n" >> slack-message.txt
 
 # Check if we need to intergoate a specific run or all of the runs for that workflow 
 if [[ -z "${run}" ]];
