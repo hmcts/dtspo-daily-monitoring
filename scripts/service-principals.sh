9080d29c0bbdc209b16beec6e8a559cf31bc42c7
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-02-08 16:33:53 +0000

    update sp script

--- scripts/service-principals.sh
+++ scripts/service-principals.sh
@@ -6,20 +6,25 @@ TODAY_DATE=$(date +%Y-%m-%d)
 
 CHECK_DATE=$(date -d "+${CHECK_DAYS} days" +%Y-%m-%d)
 
+DOMAIN=$(az rest --method get --url https://graph.microsoft.com/v1.0/domains --query 'value[?isDefault].id' -o tsv)
 
-AZ_APP_RESULT=$( az ad app list --all --query "[?passwordCredentials[?endDateTime < '${CHECK_DATE}']].{displayName:displayName, appId:appId, createdDateTime:createdDateTime, passwordCredentials:passwordCredentials[?endDateTime < '${CHECK_DATE}'].{displayName:displayName,endDateTime:endDateTime}}" --output json )
-
+if [ $DOMAIN = "HMCTS.NET" ]; then
+    AZ_APP_RESULT=$( az ad app list --all --query "[?passwordCredentials[?endDateTime < '${CHECK_DATE}']].{displayName:displayName, appId:appId, createdDateTime:createdDateTime, passwordCredentials:passwordCredentials[?endDateTime < '${CHECK_DATE}'].{displayName:displayName,endDateTime:endDateTime}}" --output json )
+else
+    AZ_APP_RESULT=$( az ad app list --display-name "DTS Operations Bootstrap GA" --query "[?passwordCredentials[?endDateTime < '${CHECK_DATE}']].{displayName:displayName, appId:appId, createdDateTime:createdDateTime, passwordCredentials:passwordCredentials[?endDateTime < '${CHECK_DATE}'].{displayName:displayName,endDateTime:endDateTime}}" --output json )
 
 AZ_APP_COUNT=$(jq -r '. | length' <<< "${AZ_APP_RESULT}")
 
-printf "\n:azure-826: <https://portal.azure.com/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/RegisteredApps|_*Service Principal Secrets Status*_> \n" >> slack-message.txt
+if [ $DOMAIN = "HMCTS.NET" ]; then
+    printf "\n:azure-826: <https://portal.azure.com/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/RegisteredApps|_*Service Principal Secrets Status*_> \n" >> slack-message.txt
+else
+    printf "\n:azure-826: <https://portal.azure.com/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/RegisteredApps|_*Service Principal Secrets Status - HMCTS Dev Tenant*_> \n" >> slack-message.txt
+fi
 
 if [[ $AZ_APP_COUNT == 0 ]]; then
     printf "\n:green_circle: No Service Principals Secrets are expiring in $CHECK_DAYS days \n\n" >> slack-message.txt
     exit 0
 fi
-
-
 
 echo "$AZ_APP_RESULT" | jq -c -r '.[]'  | while read i; do
     displayName=$(jq -r '.displayName' <<< "$i")
