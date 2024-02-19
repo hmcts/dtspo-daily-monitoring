0cc15004d9249f45e6e96e1a59dca50896975109
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-02-19 11:54:25 +0000

    echo dates

--- scripts/github_pages_checks.sh
+++ scripts/github_pages_checks.sh
@@ -5,7 +5,9 @@
 # Note, if you are running this script on MacOS, the BSD date command works differently. Use `gdate` to get the same output as below.
 
 CURRENTDATE=$(date +"%Y-%m-%d") # 2w = 2 weeks
+echo "current date is ${CURRENTDATE}"
 EXPIRETHRESHOLD=$(date -u +"%Y-%m-%d" -d "+2 weeks") # 2w = 2 weeks
+echo "expire threshold is ${EXPIRETHRESHOLD}"
 
 declare -a URLS=("https://hmcts.github.io/api/pages.json" "https://hmcts.github.io/ops-runbooks/api/pages.json")
 declare -a PAGES
