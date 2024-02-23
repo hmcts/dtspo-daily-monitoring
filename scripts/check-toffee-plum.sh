0448b6389334829a96db650093f1b700e61af576
Joshua Glass <joshua.glass@justice.gov.uk>
2024-02-23 14:07:02 +0000

    DTSPO-16429, testng assicated array output

--- scripts/check-toffee-plum.sh
+++ scripts/check-toffee-plum.sh
@@ -4,6 +4,7 @@ function add_environments() {
     if [[ "$1" == "Toffee" ]]; then
         ENVIRONMENTS=("Sandbox" "Test" "ITHC" "Demo" "Staging" "Prod")
     fi
+
     if [[ "$1" == "Plum" ]]; then
         ENVIRONMENTS=("Sandbox" "Perftest" "ITHC" "Demo" "AAT" "Prod")
     fi
@@ -22,10 +23,9 @@ function status_code() {
 
 function failure_check() {
     if [[ $statuscode != 200 ]] && [[ $1 == "Toffee" ]]; then
-        #if [[ $statuscode != 200 ]] && [[ $1 == "Toffee" ]] && [[ $failures_exist_toffee == "true" ]]; then
-            # printf "\n>:red_circle:  <$url| $ENV> is unhealthy" >>slack-message.txt
-        Toffee[failure_msg]=( "\n>:red_circle:  <$url| $ENV> is unhealthy" )
-        #fi
+        if [[ $statuscode != 200 ]] && [[ $1 == "Toffee" ]] && [[ $failures_exist_toffee == "true" ]]; then
+            printf "\n>:red_circle:  <$url| $ENV> is unhealthy" >>slack-message.txt
+        fi
         failures_exist_toffee="true"
 
 
@@ -38,7 +38,7 @@ function failure_check() {
 }
 
 function uptime() {
-    for ENV in ${$1[env[@]]}; do
+    for ENV in ${ENVIRONMENTS[@]}; do
         status_code $1 # passing name
         failure_check $1 # passing name 
     done
@@ -47,7 +47,8 @@ function uptime() {
 function do_failures_exist() {
     if [[ $1 = "Toffee" ]]; then
         if [[ $failures_exist_toffee != "true" ]]; then
-            printf "\n>:green_circle:  All environments in $1 are healthy" >>slack-message.txt
+            Toffee[no_fail_msg]+="\n>:green_circle:  All environments in $1 are healthy" 
+            Toffee[no_fail_msg]+="this is testing append on associated array"
         fi
 
     elif [[ $1 = "Plum" ]]; then
@@ -57,44 +58,45 @@ function do_failures_exist() {
     fi
 }
 
+function check_status () {
+    printf "\n*$1 Status:*" >>slack-message.txt
+    add_environments $1
+    uptime $1
+    do_failures_exist $1
+}
+
 
 # function runner
 APPS=("Toffee" "Plum")
 printf "\n:detective-pikachu: _*Check Toffee/Plum Status*_ \n" >>slack-message.txt
 
 # Check app status first before output
-# for APP in ${APPS[@]}; do
-#     # add_environments $APP # not needed now 
-#     uptime $APP
-# done
+for APP in ${APPS[@]}; do
+    # add_environments $APP
+    # uptime $APP
 
-# # format output
+    check_status $APP
+done
+printf "%s\n" "${Toffee[no_fail_msg][@]}" >>slack-message.txt
+
+
+# format output, print if no failure exist
 # if [[ $failures_exist_toffee != "true" && $failures_exist_plum != "true" ]]; then
-#     # print if no failure exist
 #     printf "\n>:green_circle:  All environments are healthy" >>slack-message.txt
 # else
+
 #     # print if failure exist, $1 data does not persist need to re run logic
 #     for APP in ${APPS[@]}; do
-#         printf "\n*$APP Status:*" >>slack-message.txt
-#         add_environments $APP
-#         uptime $APP
-#         do_failures_exist $APP
+#         check_status $APP
 #     done
 # fi
 
 # ------------
 declare -A Toffee=( 
-[app]="Toffee" 
-[env]=["Sandbox" "Test" "ITHC" "Demo" "Staging" "Prod"] 
-[failure]="False"
-[failure_msg]=""
+[no_fail_msg]=
 )
 
-uptime ${Toffee[app]}
-printf "%s\nHashMap ${Toffee[app]}: failures ${Toffee[failure]}, env ${Toffee["env[@]]}" >>slack-message.txt
 
 # need to run uptime on both arrays
 # need to check for failure, if not then simple printf else well think of that later
 
-# declare -A plum=( [app]="Plum" [env]=["Sandbox" "Perftest" "ITHC" "Demo" "AAT" "Prod"] [failure]="False" )
-# ------------
