504b9e2ebf89713ada88b8a28cb139cb56cd8e61
Joshua Glass <joshua.glass@justice.gov.uk>
2024-02-26 11:37:34 +0000

    DTSPO-16429, testing new logic

--- scripts/check-toffee-plum.sh
+++ scripts/check-toffee-plum.sh
@@ -23,36 +23,36 @@ function status_code() {
 
 function failure_check() {
     if [[ $statuscode != 200 ]] && [[ $1 == "Toffee" ]]; then
-        if [[ $statuscode != 200 ]] && [[ $1 == "Toffee" ]] && [[ $failures_exist_toffee == "true" ]]; then
-            printf "\n>:red_circle:  <$url| $ENV> is unhealthy" >>slack-message.txt
-        fi
+        # if [[ $statuscode != 200 ]] && [[ $1 == "Toffee" ]] && [[ $failures_exist_toffee == "true" ]]; then
+        toffee_failure_msg="\n>:red_circle:  <$url| $ENV> is unhealthy"
+        # fi
         failures_exist_toffee="true"
 
 
     elif [[ $statuscode != 200 ]] && [[ $1 == "Plum" ]]; then
-        if [[ $statuscode != 200 ]] && [[ $1 == "Plum" ]] && [[ $failures_exist_plum == "true" ]]; then
-            printf "\n>:red_circle:  <$url| $ENV> is unhealthy" >>slack-message.txt
-        fi
+        # if [[ $statuscode != 200 ]] && [[ $1 == "Plum" ]] && [[ $failures_exist_plum == "true" ]]; then
+        plum_failure_msg="\n>:red_circle:  <$url| $ENV> is unhealthy"
+        # fi
         failures_exist_plum="true"
     fi
 }
 
 function uptime() {
     for ENV in ${ENVIRONMENTS[@]}; do
-        status_code $1 # passing name
-        failure_check $1 # passing name 
+        status_code $1
+        failure_check $1 
     done
 }
 
 function do_failures_exist() {
     if [[ $1 = "Toffee" ]]; then
         if [[ $failures_exist_toffee != "true" ]]; then
-            Toffee[no_fail_msg]+="\n>:green_circle:  All environments in $1 are healthy" 
+            toffee_no_failure_msg="\n>:green_circle:  All environments in $1 are healthy" 
         fi
 
     elif [[ $1 = "Plum" ]]; then
         if [[ $failures_exist_plum != "true" ]]; then
-            Plum[no_fail_msg]+="\n>:green_circle:  All environments in $1 are healthy"
+            plum_no_failure_msg="\n>:green_circle:  All environments in $1 are healthy"
         fi
     fi
 }
@@ -65,38 +65,37 @@ function check_status() {
 
 function format_status() {
     # first check that no failures have occured
-    if [[ -n "${Toffee[failure_msg]}" ]] && [[ -n "${Plum[no_fail_msg]}" ]]; then
+    if [[ $failures_exist_toffee != "true" ]] && [[ $failures_exist_plum != "true" ]]; then
         printf "\n>:green_circle:  All environments are healthy" >>slack-message.txt
 
-    # if failure occurs print failure msg for each toffee or plum
-    elif [[ $1 == "Toffee" ]]; then
-        printf "\n*$1 Status:*" >>slack-message.txt
-
-        # if toffee has no failures
-        if [[ -n "${Toffee[no_fail_msg]}" ]]; then
-            printf "s%" "${Toffee[failure_msg]}" >>slack-message.txt
+    # if failure occurs print failure msg for each toffee and plum
+    else
+        printf "\n*Toffee Status:*" >>slack-message.txt
+        # might need a for loop here
+        if [[ $failures_exist_toffee != "true" ]]; then
+            printf "s%\n" "${toffee_no_failure_msg[@]}" >>slack-message.txt
         else
-            printf "s%" "${Toffee[failure_msg]}" >>slack-message.txt
+            printf "s%\n" "${toffee_failure_msg[@]}" >>slack-message.txt
+        fi
+
+        printf "\n*Plum Status:*" >>slack-message.txt
+
+        if [[ $failures_exist_plum != "true" ]]; then
+            printf "s%\n" "${plum_no_failure_msg[@]}" >>slack-message.txt
+        else
+            printf "s%\n" "${plum_failure_msg[@]}" >>slack-message.txt
         fi
     fi
-
-    # elif [[ $1 == "Plum" ]]; then
-    #     printf "\n*$1 Status:*" >>slack-message.txt
-    #     printf "s%\n" "${Plum[no_fail_msg]}" >>slack-message.txt
-    # fi
 }
 
 # hold any failure or success messages 
-declare -A Toffee=( 
-    [failure_msg]=
-    [no_fail_msg]=
-)
-declare -A Plum=( 
-    [failure_msg]=
-    [no_fail_msg]=
-)
+toffee_failure_msg=()
+toffee_no_failure_msg=()
 
-# function runner
+plum_failure_msg=()
+plum_no_failure_msg=()
+
+
 APPS=("Toffee" "Plum")
 printf "\n:detective-pikachu: _*Check Toffee/Plum Status*_ \n" >>slack-message.txt
 
@@ -105,7 +104,10 @@ for APP in ${APPS[@]}; do
     check_status $APP
 done
 
+format_status
+
+# check toffee
+# 
+# check plum 
+# 
 # format output
-for APP in ${APPS[@]}; do
-    format_status $APP
-done
