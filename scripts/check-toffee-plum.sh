8c877db0b6370c346bd4029b68153ddbd32d4c1e
Joshua Glass <joshua.glass@justice.gov.uk>
2024-02-27 15:42:36 +0000

    DTSPO-16429, removed whitespace, trying new logic

--- scripts/check-toffee-plum.sh
+++ scripts/check-toffee-plum.sh
@@ -4,7 +4,6 @@ function add_environments() {
     if [[ "$1" == "Toffee" ]]; then
         ENVIRONMENTS=("Sandbox" "Test" "ITHC" "Demo" "Staging" "Prod")
     fi
-
     if [[ "$1" == "Plum" ]]; then
         ENVIRONMENTS=("Sandbox" "Perftest" "ITHC" "Demo" "AAT" "Prod")
     fi
@@ -14,7 +13,6 @@ function status_code() {
     if [ $ENV == "Prod" ]; then
         url="https://$1.platform.hmcts.net"
         statuscode=$(curl -s -o /dev/null -w "%{http_code}" $url)
-
     elif [ $ENV != "Prod" ]; then
         url="https://$1.$ENV.platform.hmcts.net"
         statuscode=$(curl -s -o /dev/null -w "%{http_code}" $url)
@@ -22,12 +20,10 @@ function status_code() {
 }
 
 function failure_check() {
-    if [[ $statuscode != 200 ]] && [[ $1 == "Toffee" ]]; then
-        failure_msg_toffee="\n>:red_circle:  <$url| $ENV> is unhealthy"
-        # failure_msg_toffee+="\n>:green_circle:  <$url| $ENV> is healthy"
+    if [[ $statuscode == 200 ]] && [[ $1 == "Toffee" ]]; then
+        #failure_msg_toffee+="\n>:red_circle:  <$url| $ENV> is unhealthy"
+        failure_msg_toffee+="\n>:green_circle:  <$url| $ENV> is healthy"
         failures_exist_toffee="true"
-
-
     elif [[ $statuscode != 200 ]] && [[ $1 == "Plum" ]]; then
         failure_msg_plum+="\n>:red_circle:  <$url| $ENV> is unhealthy"
         failures_exist_plum="true"
@@ -37,7 +33,7 @@ function failure_check() {
 function uptime() {
     for ENV in ${ENVIRONMENTS[@]}; do
         status_code $1
-        failure_check $1 
+        failure_check $1
     done
 }
 
@@ -50,26 +46,39 @@ function success_msg() {
     printf "\n>:green_circle:  All environments are healthy" >>slack-message.txt
 }
 
-function format_failure() {
-    printf "\n*Toffee Status:*" >>slack-message.txt
-    if [[ $failures_exist_toffee == "true" ]]; then
-        printf '%s\n' "${failure_msg_toffee[@]}" >>slack-message.txt
-    else
-        success_msg
-    fi
+# function format_failure() {
+#     printf "\n*Toffee Status:*" >>slack-message.txt
+#     if [[ $failures_exist_toffee == "true" ]]; then
+#         printf '%s\n' "${failure_msg_toffee[@]}" >>slack-message.txt
+#     else
+#         success_msg
+#     fi
 
-    printf "\n*Plum Status:*" >>slack-message.txt
-    if [[ $failures_exist_plum == "true" ]]; then
-        printf '%s\n' "${failure_msg_plum[@]}" >>slack-message.txt
+#     printf "\n*Plum Status:*" >>slack-message.txt
+#     if [[ $failures_exist_plum == "true" ]]; then
+#         printf '%s\n' "${failure_msg_plum[@]}" >>slack-message.txt
+#     else
+#         success_msg
+#     fi
+# }
+
+function format_failure() {
+    local app=$1
+    local failure_exists=$2
+    local failure_msg=$3
+
+    printf "\n*$app Status:*" >>slack-message.txt
+    if [[ $failures_exist == "true" ]]; then
+        printf '%s\n' "${failure_msg[@]}" >>slack-message.txt
     else
         success_msg
     fi
 }
 
 function format_status() {
-    # if failure exists
-    if [[ $failures_exist_toffee == "true" ]] || [[ $failures_exist_plum == "true" ]]; then
-        format_failure
+    if [[ $failures_exist_toffee || $failures_exist_plum ]]; then
+        format_failure "Toffee" $failures_exist_toffee "${failure_msg_toffee[@]}"
+        format_failure "Plum" $failures_exist_plum "${failure_msg_plum[@]}"
     else
         success_msg
     fi
