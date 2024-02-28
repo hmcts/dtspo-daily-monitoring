995cff0f5834480246a7c88cf9b12962fb86d74e
Joshua Glass <141819030+JoshuaGlassKainos@users.noreply.github.com>
2024-02-28 15:06:45 +0000

    DTSPO-16429-compress-checks (#64)
    * DTSPO-16429, complete refactor
    
    ---------
    
    Signed-off-by: Joshua Glass <joshua.glass@justice.gov.uk>
    Co-authored-by: Rees Pozzi <rees.pozzi@justice.gov.uk>

--- scripts/check-toffee-plum.sh
+++ scripts/check-toffee-plum.sh
@@ -21,11 +21,11 @@ function status_code() {
 
 function failure_check() {
     if [[ $statuscode != 200 ]] && [[ $1 == "Toffee" ]]; then
+        failure_msg_toffee+="\n>:red_circle:  <$url| $ENV> is unhealthy"
         failures_exist_toffee="true"
-        printf "\n>:red_circle:  <$url| $ENV> is unhealthy" >>slack-message.txt
     elif [[ $statuscode != 200 ]] && [[ $1 == "Plum" ]]; then
+        failure_msg_plum+="\n>:red_circle:  <$url| $ENV> is unhealthy"
         failures_exist_plum="true"
-        printf "\n>:red_circle:  <$url| $ENV> is unhealthy" >>slack-message.txt
     fi
 }
 
@@ -36,24 +36,42 @@ function uptime() {
     done
 }
 
-function do_failures_exist() {
-    if [[ $1 = "Toffee" ]]; then
-        if [[ $failures_exist_toffee != "true" ]]; then
-            printf "\n>:green_circle:  All environments in $1 are healthy" >>slack-message.txt
-        fi
-    elif [[ $1 = "Plum" ]]; then
-        if [[ $failures_exist_plum != "true" ]]; then
-            printf "\n>:green_circle:  All environments in $1 are healthy" >>slack-message.txt
-        fi
+function check_status() {
+    add_environments $1
+    uptime $1
+}
+
+function format_failure() {
+    local app=$1
+    local failure_exist=$2
+    local failure_msg=$3
+
+    printf "\n*$app Status:*" >>slack-message.txt
+    if [[ $failure_exist ]]; then
+        printf '%s\n' "${failure_msg[@]}" >>slack-message.txt
+    else
+        printf "\n>:green_circle:  All environments in ${app} are healthy" >>slack-message.txt
     fi
 }
 
-printf "\n:detective-pikachu: _*Check Toffee/Plum Status*_ \n" >>slack-message.txt
+function format_status() {
+    if [[ $failures_exist_toffee || $failures_exist_plum ]]; then
+        format_failure "Toffee" $failures_exist_toffee "${failure_msg_toffee[@]}"
+        format_failure "Plum" $failures_exist_plum "${failure_msg_plum[@]}"
+    else
+        printf "\n>:green_circle:  All environments are healthy" >>slack-message.txt
+    fi
+}
+
+# hold any failure messages
+failure_msg_toffee=()
+failure_msg_plum=()
 
 APPS=("Toffee" "Plum")
+printf "\n:detective-pikachu: _*Check Toffee/Plum Status*_ \n" >>slack-message.txt
+# Check app status first
 for APP in ${APPS[@]}; do
-    printf "\n*$APP Status:*" >>slack-message.txt
-    add_environments $APP
-    uptime $APP
-    do_failures_exist $APP
+    check_status $APP
 done
+# format the output, if toffee or plum experience faults
+format_status
