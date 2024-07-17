710aa1b525ed867e7d50767ed02bd2f73ed09454
Alex Bance <Alex.Bance@justice.gov.uk>
2024-07-17 13:42:03 +0100

    feat: test with lower thresholds

--- scripts/postgres-flexible-storage.sh
+++ scripts/postgres-flexible-storage.sh
@@ -1,4 +1,4 @@
-set -ex
+set -e
 
 SUBSCRIPTION=$1
 
@@ -14,13 +14,13 @@ for ((INDEX = 0; INDEX < $COUNT; INDEX++)); do
   INSTANCE_NAME=$(echo $POSTGRES_FLEXIBLE_INSTANCES | jq -r '.['$INDEX'].name')
   INSTANCE_URL="https://portal.azure.com/#@HMCTS.NET/resource$INSTANCE_ID"
   STORAGE_USED=$(az monitor metrics list --resource "$INSTANCE_ID" --metric storage_percent --offset 0d6h --interval 6h | jq -r ".value[0].timeseries[0].data[0].average")
-  if [ $STORAGE_USED -gt 95 ]; then
-    echo "> :red_circle: <$INSTANCE_URL|_*$INSTANCE_NAME*_> is running above 95% storage capacity at *$STORAGE_USED%*" >>slack-message.txt
-  elif [ $STORAGE_USED -gt 80 ]; then
-    echo "> :yellow_circle: <$INSTANCE_URL|_*$INSTANCE_NAME*_> is running above 80% storage capacity at *$STORAGE_USED%*" >>slack-message.txt
+  if [ $STORAGE_USED -gt 60 ]; then
+    echo "> :red_circle: <$INSTANCE_URL|_*$INSTANCE_NAME*_> is running above 60% storage capacity at *$STORAGE_USED%*" >>slack-message.txt
+  elif [ $STORAGE_USED -gt 30 ]; then
+    echo "> :yellow_circle: <$INSTANCE_URL|_*$INSTANCE_NAME*_> is running above 30% storage capacity at *$STORAGE_USED%*" >>slack-message.txt
   else
     STORAGE_SAFE_COUNT=$(($STORAGE_SAFE_COUNT+1))
   fi
 done
 
-echo "> :green_circle: $STORAGE_SAFE_COUNT PostgreSQL Flexible Servers are running below 80% storage capacity." >>slack-message.txt
+echo "> :green_circle: $STORAGE_SAFE_COUNT PostgreSQL Flexible Servers are running below 30% storage capacity." >>slack-message.txt
