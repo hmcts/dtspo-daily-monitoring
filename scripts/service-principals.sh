c5f7bddb5a5a0fff7cb55a546db8786d17036295
Marty Fox <martin.fox1@justice.gov.uk>
2024-09-04 15:11:29 +0100

    Adding service principal check test

--- scripts/service-principals.sh
+++ scripts/service-principals.sh
@@ -28,7 +28,7 @@ EOF
 exit 1
 }
 
-args=$(getopt -a -o t:c:p:g: --long slackBotToken:,slackChannelName:,help -- "$@")
+args=$(getopt -a -o t:c:p:g: --long slackBotToken:,slackChannelName:checkDays:,help -- "$@")
 if [[ $? -gt 0 ]]; then
     usage
 fi
