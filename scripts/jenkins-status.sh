c620f4fcff70bc87e1c5183aecaf057ce73a1583
Marty Fox <martin.fox1@justice.gov.uk>
2024-11-13 10:23:27 +0000

    jenkins status jobs

--- scripts/jenkins-status.sh
+++ scripts/jenkins-status.sh
@@ -4,8 +4,6 @@ JENKINS_USERNAME=$1
 JENKINS_API_TOKEN=$2
 JENKINS_URL=$3
 
-printf "\n:jenkins: <https://build.platform.hmcts.net/|_*Jenkins Status*_> \n\n" >> slack-message.txt
-
 BUILD_QUEUE_RESULT=$( curl -u "$JENKINS_USERNAME":"$JENKINS_API_TOKEN" "$JENKINS_URL/queue/api/json")
 BUILD_QUEUE_COUNT=$(jq -r '.items | length' <<< "$BUILD_QUEUE_RESULT")
 
@@ -18,7 +16,10 @@ fi
 
 printf ">_%s  Build Queue :_ *%s* :sign-queue: \n" "$BUILD_QUEUE_STATUS" "$BUILD_QUEUE_COUNT" >> slack-message.txt
 
-printf ">\n> _Dashboard Status:_  \n>\n" >> slack-message.txt
+# Post initial header message
+slackNotification $slackBotToken $slackChannelName ":jenkins: Jenkins Status" "$BUILD_QUEUE_STATUS _Build Queue :_ *$BUILD_QUEUE_COUNT* :sign-queue:"
+
+slackThreadResponse $slackBotToken $slackChannelName "_Dashboard Status:_" $TS
 
 DASHBOARD_RESULT=$( curl -u $JENKINS_USERNAME:$JENKINS_API_TOKEN "$JENKINS_URL/view/Platform/api/json?depth=1")
 
@@ -35,6 +36,7 @@ for ((i=0; i< ${count}; i++)); do
     elif [[ "$COLOR" == "blue" ]]; then
     BUILD_STATUS=":green_circle:"
     fi
-  printf "> %s <%s|%s> \n" "$BUILD_STATUS" "$URL" "$FULL_DISPLAY_NAME" >> slack-message.txt
+  slackThreadResponse $slackBotToken $slackChannelName "$BUILD_STATUS <$URL|$FULL_DISPLAY_NAME> \n" $TS
 done
+
 
