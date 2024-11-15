f40fe63efb0c6152c7675e52ab815659b1357e4d
Marty Fox <martin.fox1@justice.gov.uk>
2024-11-15 12:34:48 +0000

    Update Jenkins script to use slack common functions

--- scripts/jenkins-status.sh
+++ scripts/jenkins-status.sh
@@ -1,12 +1,70 @@
-set -ex
+#!/usr/bin/env bash
 
-JENKINS_USERNAME=$1
-JENKINS_API_TOKEN=$2
-JENKINS_URL=$3
+### Setup script environment
+set -exuo pipefail
 
-printf "\n:jenkins: <https://build.platform.hmcts.net/|_*Jenkins Status*_> \n\n" >> slack-message.txt
+# Source central functions script
+source scripts/common-functions.sh
 
-BUILD_QUEUE_RESULT=$( curl -u "$JENKINS_USERNAME":"$JENKINS_API_TOKEN" "$JENKINS_URL/queue/api/json")
+slackBotToken=
+slackChannelName=
+jenkinsUsername=
+jenkinsApiToken=
+jenkinsURL=
+
+usage(){
+>&2 cat << EOF
+------------------------------------------------
+Script to check GitHub page expiry
+------------------------------------------------
+Usage: $0
+    [ -t | --slackBotToken ]
+    [ -c | --slackChannelName ]
+    [ -j | --jenkinsUsername ]
+    [ -t | --jenkinsApiToken ]
+    [ -u | --jenkinsURL ]
+    [ -h | --help ]
+EOF
+exit 1
+}
+
+args=$(getopt -a -o t:c:j:t:u:h: --long slackBotToken:,slackChannelName:,jenkinsUsername:,jenkinsApiToken:,jenkinsURL:,help -- "$@")
+if [[ $? -gt 0 ]]; then
+    usage
+fi
+
+eval set -- ${args}
+while :
+do
+    case $1 in
+        -h | --help)              usage                    ; shift   ;;
+        -t | --slackBotToken)     slackBotToken=$2         ; shift 2 ;;
+        -c | --slackChannelName)  slackChannelName=$2      ; shift 2 ;;
+        -j | --jenkinsUsername)   jenkinsUsername=$2       ; shift 2 ;;
+        -t | --jenkinsApiToken)   jenkinsApiToken=$2       ; shift 2 ;;
+        -u | --jenkinsURL)        jenkinsURL=$2            ; shift 2 ;;
+        # -- means the end of the arguments; drop this, and break out of the while loop
+        --) shift; break ;;
+        *) >&2 echo Unsupported option: $1
+            usage ;;
+    esac
+done
+
+if [[ -z "$slackBotToken" || -z "$slackChannelName" || -z "$jenkinsUsername" || -z "$jenkinsApiToken" || -z "$jenkinsURL" ]]; then
+    {
+      echo "------------------------"
+      echo 'Please supply all of: '
+      echo '- Slack token'
+      echo '- Slack channel name'
+      echo '- Jenkins Username'
+      echo '- Jenkins API Token'
+      echo '- Jenkins URL'
+      echo "------------------------"
+    } >&2
+    exit 1
+fi
+
+BUILD_QUEUE_RESULT=$( curl -u "$jenkinsUsername":"$jenkinsApiToken" "$jenkinsURL/queue/api/json")
 BUILD_QUEUE_COUNT=$(jq -r '.items | length' <<< "$BUILD_QUEUE_RESULT")
 
 BUILD_QUEUE_STATUS=":red_circle:"
@@ -16,14 +74,11 @@ elif (( "$BUILD_QUEUE_COUNT" <= 125 )); then
   BUILD_QUEUE_STATUS=":yellow_circle:"
 fi
 
-printf ">_%s  Build Queue :_ *%s* :sign-queue: \n" "$BUILD_QUEUE_STATUS" "$BUILD_QUEUE_COUNT" >> slack-message.txt
-
-printf ">\n> _Dashboard Status:_  \n>\n" >> slack-message.txt
-
-DASHBOARD_RESULT=$( curl -u $JENKINS_USERNAME:$JENKINS_API_TOKEN "$JENKINS_URL/view/Platform/api/json?depth=1")
+DASHBOARD_RESULT=$( curl -u $jenkinsUsername:$jenkinsApiToken "$jenkinsURL/view/Platform/api/json?depth=1")
 
 count=$(jq -r '.jobs | length' <<< $DASHBOARD_RESULT)
 
+BUILD_STATUS_RESULTS=()
 for ((i=0; i< ${count}; i++)); do
     URL=$(jq -r '.jobs['$i'].url' <<< "$DASHBOARD_RESULT")
     COLOR=$(jq -r '.jobs['$i'].color' <<< "$DASHBOARD_RESULT")
@@ -35,6 +90,21 @@ for ((i=0; i< ${count}; i++)); do
     elif [[ "$COLOR" == "blue" ]]; then
     BUILD_STATUS=":green_circle:"
     fi
-  printf "> %s <%s|%s> \n" "$BUILD_STATUS" "$URL" "$FULL_DISPLAY_NAME" >> slack-message.txt
+
+    BUILD_STATUS_RESULTS+=("$BUILD_STATUS <$URL|$FULL_DISPLAY_NAME>")
 done
 
+CHECK_STATUS=":green_circle:"
+for result in "${BUILD_STATUS_RESULTS[@]}"; do
+    if [[ "$result" == *"red_circle"* ]]; then
+      CHECK_STATUS=":red_circle:"
+      break
+    elif [[ "$result" == *"yellow_circle"* ]]; then
+      CHECK_STATUS=":yellow_circle:"
+    fi
+done
+
+# Post initial header message
+slackNotification $slackBotToken $slackChannelName ":jenkins: $CHECK_STATUS Jenkins Status" "$BUILD_QUEUE_STATUS _Build Queue :_ *$BUILD_QUEUE_COUNT* :sign-queue:"
+#Dashboard Status
+slackThreadResponse $slackBotToken $slackChannelName "_Dashboard Status:_\n$(printf "%s\n" "${BUILD_STATUS_RESULTS[@]}")" $TS
