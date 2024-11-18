1a09fdde1e3e28985564049dce82854bc272c675
Marty Fox <martin.fox1@justice.gov.uk>
2024-11-18 09:44:25 +0000

    Updating github pages check for threads and pipeline order

--- scripts/github_pages_checks.sh
+++ scripts/github_pages_checks.sh
@@ -1,26 +1,65 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script will curl the output of the pages found in the URLS array and use JQ to parse the output for specific information
 # The URLs must return valid JSON for JQ to parse it and be of a similar format to those found below i.e. GitHub pages API output.
 # Note, if you are running this script on MacOS, the BSD date command works differently. Use `gdate` to get the same output as below.
 
-# Check platform
-platform=$(uname)
+### Setup script environment
+set -euo pipefail
 
-# Check and install missing packages
-if [[ $platform == "Darwin" ]]; then
-    date_command=$(which gdate)
-elif [[ $platform == "Linux" ]]; then
-    date_command=$(which date)
+# Source central functions script
+source scripts/common-functions.sh
+
+slackBotToken=
+slackChannelName=
+
+usage(){
+>&2 cat << EOF
+------------------------------------------------
+Script to check GitHub page expiry
+------------------------------------------------
+Usage: $0
+    [ -t | --slackBotToken ]
+    [ -c | --slackChannelName ]
+    [ -h | --help ]
+EOF
+exit 1
+}
+
+args=$(getopt -a -o t:c:p:g: --long slackBotToken:,slackChannelName:,help -- "$@")
+if [[ $? -gt 0 ]]; then
+    usage
 fi
 
+eval set -- ${args}
+while :
+do
+    case $1 in
+        -h | --help)              usage                    ; shift   ;;
+        -t | --slackBotToken)     slackBotToken=$2         ; shift 2 ;;
+        -c | --slackChannelName)  slackChannelName=$2      ; shift 2 ;;
+        # -- means the end of the arguments; drop this, and break out of the while loop
+        --) shift; break ;;
+        *) >&2 echo Unsupported option: $1
+            usage ;;
+    esac
+done
+
+if [[ -z "$slackBotToken" || -z "$slackChannelName" ]]; then
+    echo "------------------------"
+    echo 'Please supply a Slack token and a Slack channel name' >&2
+    echo "------------------------"
+    exit 1
+fi
+
+### Script begins
 CURRENTDATE=$($date_command +"%Y-%m-%d") # 2w = 2 weeks
 EXPIRETHRESHOLD=$($date_command -u +"%Y-%m-%d" -d "+2 weeks") # 2w = 2 weeks
 
 declare -a URLS=("https://hmcts.github.io/api/pages.json" "https://hmcts.github.io/ops-runbooks/api/pages.json")
 declare -a PAGES
 
-# scrapeURLs will loop over each URL found in the URLS array and replace the short form URLs in the records 
+# scrapeURLs will loop over each URL found in the URLS array and replace the short form URLs in the records
 # with a complete URL that it will then save into an array called PAGES
 function scrapeUrls() {
     for URL in ${URLS[@]}; do
@@ -34,11 +73,13 @@ function scrapeUrls() {
 # If found it will print a list of the pages found in a slack hyperlink format so it is clickable
 function findNullUrls() {
 
-    NULLFOUNDURLs=$(jq -rc '. | select(.review_by == null) | "<" + .url + "|" + .title + ">"' <<<$PAGES) 
+    NULLFOUNDURLs=$(jq -rc '. | select(.review_by == null) | "> "+"<" + .url + "|" + .title + ">"' <<<$PAGES)
 
     if [ -n "$NULLFOUNDURLs" ]; then
-        printf ">:red_circle: Pages found with no review date set: \n\n" >> slack-message.txt
-        printf "%s\n\n" "$NULLFOUNDURLs" | tr -d '"' >> slack-message.txt
+        local URLS=$(printf "%s\n\n" "$NULLFOUNDURLs" | tr -d '"')
+
+        slackThreadResponse $slackBotToken $slackChannelName ":red_circle: Pages found with no review date set! \\n$URLS" $TS
+        # slackThreadResponse $slackBotToken $slackChannelName "$URLS" $TS #$TS is an output of the slackNotification function
     fi
 }
 
@@ -46,11 +87,13 @@ function findNullUrls() {
 # If found it will print a list of the pages found in a slack hyperlink format so it is clickable
 function findExpiredUrls() {
 
-    EXPIREDFOUNDURLs=$(jq -c '. | select(.review_by != null and .review_by < "'$CURRENTDATE'") | "<" + .url + "|" + .title + ">"' <<<$PAGES)
+    EXPIREDFOUNDURLs=$(jq -c '. | select(.review_by != null and .review_by < "'$CURRENTDATE'") | "> "+"<" + .url + "|" + .title + ">"' <<<$PAGES)
 
     if [ -n "$EXPIREDFOUNDURLs" ]; then
-        printf "\n>:red_circle: Pages found which have an expired review date: \n\n" >> slack-message.txt
-        printf "%s\n\n" "$EXPIREDFOUNDURLs" | tr -d '"' >> slack-message.txt
+        local URLS=$(printf "%s\n\n" "$EXPIREDFOUNDURLs" | tr -d '"')
+
+        slackThreadResponse $slackBotToken $slackChannelName ":red_circle: Pages found which have an expired review date! \\n$URLS" $TS
+        # slackThreadResponse $slackBotToken $slackChannelName "$URLS" $TS #$TS is an output of the slackNotification function
     fi
 }
 
@@ -62,19 +105,26 @@ function findExpiringUrls() {
     EXPIRINGFOUNDURLs=$(jq -c '. | select(.review_by != null and .review_by < "'$EXPIRETHRESHOLD'" and .review_by > "'$CURRENTDATE'") | "> "+"<" + .url + "|" + .title + ">"' <<<$PAGES)
 
     if [ -n "$EXPIRINGFOUNDURLs" ]; then
-        printf "\n>:yellow_circle: Pages found which require a review in the next 13 days: \n\n" >> slack-message.txt
-        printf "%s\n\n" "$EXPIRINGFOUNDURLs" | tr -d '"' >> slack-message.txt
+        local URLS=$(printf "%s\n\n" "$EXPIRINGFOUNDURLs" | tr -d '"')
+
+        slackThreadResponse $slackBotToken $slackChannelName ":yellow_circle: Pages found which require a review in the next 13 days! \\n$URLS" $TS
+        # slackThreadResponse $slackBotToken $slackChannelName "$URLS" $TS
     fi
 }
 
 function findGoodUrls() {
     if [[ -z "$EXPIRINGFOUNDURLs" && -z "$EXPIREDFOUNDURLs" ]]; then
-        printf "\n>:green_circle: All pages have acceptable review dates! :smile: \n\n" >> slack-message.txt
+        slackThreadResponse $slackBotToken $slackChannelName ":green_circle: All pages have acceptable review dates! :smile:" $TS
     fi
 }
 
+# Scrape sites in list for pages to review
 scrapeUrls
-printf "\n\n:github: :document_it: <https://hmcts.github.io|*_HMCTS Way_*> and <https://hmcts.github.io/ops-runbooks|*_Ops Runbook_*> status: \n\n" >> slack-message.txt
+
+# Post initial header message
+slackNotification $slackBotToken $slackChannelName "Documentation Review" ":github: <https://hmcts.github.io|*_HMCTS Way_*> and <https://hmcts.github.io/ops-runbooks|*_Ops Runbook_*> Status :document_it:"
+
+# Run checks on scraped pages to find the review dates and outcomes for each.
 findNullUrls
 findExpiredUrls
 findExpiringUrls
