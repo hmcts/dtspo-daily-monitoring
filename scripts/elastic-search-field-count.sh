0f9c38d6677d018862443431aaf4cfa8c0f5d61d
Jake Moorhead <jake.moorhead@justice.gov.uk>
2025-02-28 15:09:13 +0000

    Adding Elastic search check to daily checks

---
+++ scripts/elastic-search-field-count.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Configuring the script
+ELASTICSEARCH_HOST="10.96.85.7:9200"
+OUTPUT=""
+
+# Get the list of all index names
+INDEX_LIST=$(curl -s -XGET "$ELASTICSEARCH_HOST/_cat/indices?h=index")
+
+#Read each index from the file
+while IFS= read -r index_name; do
+    if [[ -n "$index_name" ]]; then
+        #Get the field count for the index
+        field_count=$(curl -s -XGET "$ELASTICSEARCH_HOST/$index_name/_mapping?pretty" | grep type | wc -l)
+        index_count=$(curl -s -XGET "$ELASTICSEARCH_HOST/_cat/indices" | awk '{print $3, $7}' | grep $index_name)
+        
+        # Append the result to the output variable
+        OUTPUT+="$index_count:  Field Count - $field_count"$'\n'
+    fi
+
+done <<< "$INDEX_LIST"
+
+echo "$OUTPUT"
