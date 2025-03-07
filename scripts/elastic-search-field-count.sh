12cf8f71897f7c46ea05ccbb42d026a7902b0bf2
Jake Moorhead <jake.moorhead@justice.gov.uk>
2025-03-07 12:45:51 +0000

    fix

--- scripts/elastic-search-field-count.sh
+++ scripts/elastic-search-field-count.sh
@@ -17,7 +17,7 @@ while IFS= read -r index_name; do
         index_count=$(curl -sv -XGET "$ELASTICSEARCH_HOST/_cat/indices" | awk '{print $3, $7}' | grep $index_name)
         
         # Append the result to the output variable
-        if [[ $field_count -ge 7500; ]] then
+        if [[ $field_count -ge 7500 ]]; then
         OUTPUT+="$index_count:  Field Count - $field_count"$'\n'
         fi
     fi
