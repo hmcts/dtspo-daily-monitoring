3e95e06be15f06025e68b95d14a64c9c92e020a9
Niall King <niall.king@justice.gov.uk>
2025-03-19 11:05:13 +0000

    testing code

--- scripts/service-principals.sh
+++ scripts/service-principals.sh
@@ -57,11 +57,11 @@ DOMAIN=$(az rest --method get --url https://graph.microsoft.com/v1.0/domains --q
 
 # if [ $DOMAIN = "hmctssboxextid.onmicrosoft.com" ]; then
 # B2C Tenant Service Principal login 
-az login --service-principal --username "$(b2c-sbox-serviceprincipal-id)" --password "$(b2c-sboxserviceprincipal-password)" --tenant hmctssboxextid.onmicrosoft.com --allow-no-subscriptions
+az login --service-principal --username "${b2c_sbox_serviceprincipal_id}" --password "${b2c_sbox_serviceprincipal_password}" --tenant hmctssboxextid.onmicrosoft.com --allow-no-subscriptions
 
-echo "Service Principal ID: ${b2c-sbox-serviceprincipal-id}"
-echo "Service Principal Password: ${b2c-sboxserviceprincipal-password}"
-echo "Tenant ID: ${b2c-sbox-tenant-id}"
+echo "Service Principal ID: ${b2c_sbox_serviceprincipal_id}"
+echo "Service Principal Password: ${b2c_sbox_serviceprincipal_password}"
+echo "Tenant ID: ${b2c_sbox_tenant_id}"
 
 AZ_APP_RESULT=$( az ad app list --display-name "B2C Tenants" --query "[?passwordCredentials[?endDateTime < '${CHECK_DATE}']].{displayName:displayName, appId:appId, createdDateTime:createdDateTime, passwordCredentials:passwordCredentials[?endDateTime < '${CHECK_DATE}'].{displayName:displayName,endDateTime:endDateTime}}" --output json )
 # elif [ $DOMAIN = "HMCTS.NET" ]; then
