7c7716a8d290a8221b944a0a14571dfa023d4e9d
roger yu <roger.yu@justice.gov.uk>
2025-06-03 21:06:29 +0100

    adding JWT creation script for PR

---
+++ scripts/create-github-app-jwt.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+
+client_id=$1 # Client ID (appID) as first argument
+
+pem=$2 #take pem as string as second argument
+
+now=$(date +%s)
+iat=$((${now} - 60)) # Issues 60 seconds in the past
+exp=$((${now} + 3600)) 
+
+b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
+
+header_json='{
+    "typ":"JWT",
+    "alg":"RS256"
+}'
+# Header encode
+header=$( echo -n "${header_json}" | b64enc )
+
+payload_json="{
+    \"iat\":${iat},
+    \"exp\":${exp},
+    \"iss\":\"${client_id}\"
+}"
+# Payload encode
+payload=$( echo -n "${payload_json}" | b64enc )
+
+# Signature
+header_payload="${header}"."${payload}"
+signature=$(
+    openssl dgst -sha256 -sign <(echo -n "${pem}") \
+    <(echo -n "${header_payload}") | b64enc
+)
+
+# Create JWT
+JWT="${header_payload}"."${signature}"
+printf '%s\n' "JWT: $JWT"
+export JWT="$JWT"
