2ca629785eb6a19aa6b7af7a066eed73592255b6
roger yu <roger.yu@justice.gov.uk>
2025-06-04 23:37:41 +0100

    revert back, store cert as pem

--- scripts/create-github-app-jwt.sh
+++ scripts/create-github-app-jwt.sh
@@ -1,30 +1,18 @@
 #!/usr/bin/env bash
+# from the github docs example https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-a-json-web-token-jwt-for-a-github-app
+
 thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 set -o pipefail
 
 #input
 client_id=$1 # Client ID (appID) as first argument
-base64pem=$2 #take pem as string as second argument
+pem=$( cat $2 ) # file path of the private key as second argument
 
-# logging
-log_file="$thisdir/create-github-app-jwt.log"
-log() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$log_file"
-}
-
-#decode it back to private key
-temp_cert_file=$(mktemp)
-echo $base64pem | base64 --decode> "$temp_cert_file"
-log "temp_cert_file"
-log $(cat $temp_cert_file)
-
-#encodes in url safe base64 
-b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
-
-#header
 now=$(date +%s)
 iat=$((${now} - 60)) # Issues 60 seconds in the past
-exp=$((${now} + 3600))
+exp=$((${now} + 600)) # Expires 10 minutes in the future
+
+b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
 
 header_json='{
     "typ":"JWT",
@@ -43,26 +31,11 @@ payload=$( echo -n "${payload_json}" | b64enc )
 
 # Signature
 header_payload="${header}"."${payload}"
-
-#avoiding process substitution
-temp_header_payload_file=$(mktemp)
-echo -n "${header_payload}" > "$temp_header_payload_file"
-log "temp_header_payload_file"
-log $(cat $temp_header_payload_file)
-
 signature=$(
-    openssl dgst -sha256 -sign $temp_cert_file \
-    $temp_header_payload_file | b64enc
+    openssl dgst -sha256 -sign <(echo -n "${pem}") \
+    <(echo -n "${header_payload}") | b64enc
 )
-log "signature"
-log $signature
 
 # Create JWT
 JWT="${header_payload}"."${signature}"
-echo "##vso[task.setvariable variable=jwt;issecret=true]$JWT"
-
-log "JWT"
-log $JWT
-
-# Clean up the temporary files
-rm -f "$temp_key_file" "$temp_header_payload_file"
+printf '%s\n' "JWT: $JWT"
