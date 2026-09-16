6acbaae9b1a77e2abd6a66ea565fc7671683918d
Salim Abdulkareem <salim.abdulkareem@justice.gov.uk>
2026-09-16 10:44:10 +0100

    update the description

--- scripts/break-glass-signin-check.sh
+++ scripts/break-glass-signin-check.sh
@@ -6,7 +6,8 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/common-functions.sh"
 
 slackBotToken=
 slackChannelName=
-ACCOUNTS='felix.eyetanga@HMCTS.NET,Thomas.ThorntonGA@HMCTS.NET'
+# Update this list when GA break-glass accounts are added, removed, or changed.
+ACCOUNTS='felix.eyetanga@HMCTS.NET,Thomas.ThorntonGA@HMCTS.NET,salim.abdulkareem@HMCTS.NET'
 REPORT_DIR="${PWD}/breakglass-signin-reports"
 REPORT_FILE="$REPORT_DIR/breakglass-signin-review.csv"
 REPORT_MD="$REPORT_DIR/breakglass-signin-review.md"
